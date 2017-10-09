ab74f079f8156ccace33ac77452ced7bb133989e
Anton Khorev <tony29@yandex.ru>
2017-10-09 13:55:06 +0300

    use crnx-dita

--- src/main.js
+++ src/main.js
@@ -1,8 +1,86 @@
 'use strict'
 
+const classData={
+	shape: {
+	},
+	ellipse: {
+		parents: 'shape',
+	},
+	circle: {
+		parents: 'ellipse',
+	},
+	// polygons:
+	polygon: {
+		parents: 'shape',
+	},
+	simple_polygon: {
+		parents: 'polygon',
+	},
+	convex_polygon: {
+		parents: 'simple_polygon',
+	},
+	regular_convex_polygon: {
+		parents: 'convex_polygon',
+	},
+	// triangles:
+	triangle: {
+		parents: 'convex_polygon',
+	},
+	right_triangle: {
+		parents: 'triangle',
+	},
+	acute_triangle: {
+		parents: 'triangle',
+	},
+	obtuse_triangle: {
+		parents: 'triangle',
+	},
+	isosceles_triangle: {
+		parents: 'triangle',
+	},
+	equilateral_triangle: {
+		parents: 'regular_convex_polygon,acute_triangle,isosceles_triangle',
+	},
+	// quadrilaterals:
+	quadrilateral: {
+		parents: 'polygon',
+	},
+	simple_quadrilateral: {
+		parents: 'simple_polygon,quadrilateral',
+	},
+	convex_quadrilateral: {
+		parents: 'convex_polygon,quadrilateral',
+	},
+	trapezoid: {
+		parents: 'convex_quadrilateral',
+	},
+	isosceles_trapezoid: {
+		parents: 'trapezoid',
+	},
+	parallelogram: {
+		parents: 'trapezoid',
+	},
+	rectangle: {
+		parents: 'isosceles_trapezoid,parallelogram',
+	},
+	rhombus: {
+		parents: 'parallelogram',
+	},
+	square: {
+		parents: 'regular_convex_polygon,rectangle,rhombus',
+	},
+}
+const defaultClassColumns=['circle','shape','convex_quadrilateral','rhombus','square','rectangle','parallelogram']
+const ClassOptions=require('crnx-dita/class-options')
+const DiagramOutput=require('crnx-dita/diagram-output')
+
 $(function(){
 	$('.crnx-dita-demo-shapes').each(function(){
 		const $container=$(this)
-		$container.empty().append("Hello world")
+		const classOptions=new ClassOptions(classData,defaultClassColumns)
+		const diagramOutput=new DiagramOutput(classOptions)
+		$container.empty().append(
+			$("<table class='dita'>").append(diagramOutput.$output)
+		)
 	})
 })
