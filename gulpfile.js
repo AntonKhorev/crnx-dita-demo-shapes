f43127e3a95f3a5a7fa0ee0b956d0025151b049f
Anton Khorev <tony29@yandex.ru>
2017-10-09 11:21:35 +0300

    hello world

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const gulp=require('gulp')
+const gulpTasks=require('crnx-build/gulp-tasks')
+
+gulpTasks(
+	gulp,
+	"dita demo: shapes",
+	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/styles/default.min.css'],
+	['http://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.3.0/highlight.min.js'],
+	[] // [require.resolve('crnx-base')]
+)
---
+++ src/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+$(function(){
+	$('.crnx-dita-demo-shapes').each(function(){
+		const $container=$(this)
+		$container.empty().append("Hello world")
+	})
+})
